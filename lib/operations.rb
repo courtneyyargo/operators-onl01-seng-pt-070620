a3c554d84c459e05a418f32322dd591b2806583c
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-09 19:37:09 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,9 +3,11 @@ if speed<40 || speed>60
   true
 else 
   false
+<<<<<<< HEAD
 end
+=======
+>>>>>>> 064296b90ba33bd44aa8a586e102682461c50fdc
 end
-
 
 def not_safe?(speed)
  speed < 40 || speed > 60 ? 
